758402282d9b41024da4a09453bc5f039849357e
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-06-01 11:03:36 +0300

    better PageTemplates description

--- src/app/PageTemplates.php
+++ src/app/PageTemplates.php
@@ -10,8 +10,14 @@ trait PageTemplates
     |--------------------------------------------------------------------------
     |
     | Each page template has its own method, that define what fields should show up using the Backpack\CRUD API.
-    | Use snake_case for naming and PageManager will make sure it looks pretty in the create/update form template dropdown.
+    | Use snake_case for naming and PageManager will make sure it looks pretty in the create/update form
+    | template dropdown.
     |
+    | Any fields defined here will show up after the standard page fields:
+    | - select template
+    | - page name (only seen by admins)
+    | - page title
+    | - page slug
     */
 
     private function services()
