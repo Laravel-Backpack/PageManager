7a7a6c9c268af620a4c93ab8bf2ebcf531998f6e
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-04-05 15:49:08 +0300

    changed pagemanager config name for consistency

---
+++ src/config/pagemanager.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	// Change this class if you wish to extend PageCrudController
+    'admin_controller_class' => 'Backpack\PageManager\App\Http\Controllers\Admin\PageCrudController',
+
+    // Change this class if you wish to extend the Page model
+    'page_model_class'       => 'Backpack\PageManager\App\Models\Page',
+];
