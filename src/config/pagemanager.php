39df42d7fecedbba9b702d0acf9dc0e5ab6d2789
Carlos Rodríguez <carloscsrm@gmail.com>
2017-04-06 16:40:01 +0200

    Update pagemanager to fix issue #28
    Updated pagemanager to avoid problem "Class Backpack\PageManager\App\Http\Controllers\Admin\PageCrudController does not exist"

--- src/config/pagemanager.php
+++ src/config/pagemanager.php
@@ -2,8 +2,8 @@
 
 return [
     // Change this class if you wish to extend PageCrudController
-    'admin_controller_class' => 'Backpack\PageManager\App\Http\Controllers\Admin\PageCrudController',
+    'admin_controller_class' => 'Backpack\PageManager\app\Http\Controllers\Admin\PageCrudController',
 
     // Change this class if you wish to extend the Page model
-    'page_model_class'       => 'Backpack\PageManager\App\Models\Page',
+    'page_model_class'       => 'Backpack\PageManager\app\Models\Page',
 ];
