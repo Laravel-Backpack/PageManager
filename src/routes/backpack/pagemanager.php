5dbd87aa6c7ae55754a024ff8ed9fab31c1c03ae
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-08-16 08:04:40 +0000

    Apply fixes from StyleCI

--- src/routes/backpack/pagemanager.php
+++ src/routes/backpack/pagemanager.php
@@ -16,15 +16,15 @@ Route::group([
     ], function () {
         $controller = config('backpack.pagemanager.admin_controller_class', 'Backpack\PageManager\app\Http\Controllers\Admin\PageCrudController');
 
-    // Backpack\PageManager routes
-    Route::get('page/create/{template}', $controller.'@create');
+        // Backpack\PageManager routes
+        Route::get('page/create/{template}', $controller.'@create');
         Route::get('page/{id}/edit/{template}', $controller.'@edit');
 
-    // This triggered an error before publishing the PageTemplates trait, when calling Route::controller();
-    // CRUD::resource('page', $controller . '');
+        // This triggered an error before publishing the PageTemplates trait, when calling Route::controller();
+        // CRUD::resource('page', $controller . '');
 
-    // So for PageCrudController all routes are explicitly defined:
-    Route::get('page/reorder', $controller.'@reorder');
+        // So for PageCrudController all routes are explicitly defined:
+        Route::get('page/reorder', $controller.'@reorder');
         Route::get('page/reorder/{lang}', $controller.'@reorder');
         Route::post('page/reorder', $controller.'@saveReorder');
         Route::post('page/reorder/{lang}', $controller.'@saveReorder');
