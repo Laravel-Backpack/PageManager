a6af586f4daf2497bd4cb48424d33807e1a7790a
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-08-29 12:00:47 +0000

    Apply fixes from StyleCI

--- src/app/Http/Controllers/Admin/PageCrudController.php
+++ src/app/Http/Controllers/Admin/PageCrudController.php
@@ -14,7 +14,6 @@ class PageCrudController extends CrudController
     use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation { create as traitCreate; }
     use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation { edit as traitEdit; }
     use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-
     use PageTemplates;
 
     public function setup($template_name = false)
