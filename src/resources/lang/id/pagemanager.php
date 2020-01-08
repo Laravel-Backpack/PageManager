086ab7787b004c20657b7a6fdeee1ea2e6ed0edc
VREEdom <35516476+vonsogt@users.noreply.github.com>
2020-01-08 08:53:49 +0700

    add Indonesian language

---
+++ src/resources/lang/id/pagemanager.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'change_template_confirmation' => 'Anda yakin ingin mengubah templat halaman? Anda akan kehilangan modifikasi yang belum disimpan untuk halaman ini.',
+    'content' => 'Konten',
+    'content_placeholder' => 'Konten Anda di sini',
+    'meta_description' => 'Deskripsi meta',
+    'meta_keywords' => 'Meta Keywords',
+    'meta_title' => 'Kata Kunci Meta',
+    'metas' => 'Meta',
+    'name' => 'Nama',
+    'open' => 'Buka',
+    'page' => 'halaman',
+    'page_name' => 'Nama halaman (hanya dilihat oleh admin)',
+    'page_slug' => 'Halaman Slug (URL)',
+    'page_slug_hint' => 'Akan dibuat secara otomatis dari judul Anda, jika dibiarkan kosong.',
+    'page_title' => 'Judul halaman',
+    'pages' => 'halaman',
+    'slug' => 'Slug',
+    'template' => 'Template',
+    'template_not_found' => 'Template tidak dapat ditemukan. Mungkin sudah dihapus sejak halaman ini dibuat. Untuk melanjutkan, minta webmin atau tim pengembangan Anda untuk memperbaikinya.',
+];
