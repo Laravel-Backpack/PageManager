32d9d49a4427b05f9bc023add7b70780ce82ba6b
Selçuk Çukur <selcukcukur@outlook.com.tr>
2020-09-24 20:33:09 +0300

    Turkish language file is included.

---
+++ src/resources/lang/tr/pagemanager.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'change_template_confirmation' => 'Sayfa şablonunu değiştirmek istediğinizden eminmisiniz? Bu sayfa için uyguladığınız tüm değişiklikleri kaybedeceksiniz.',
+    'content' => 'İçerik',
+    'content_placeholder' => 'İçeriğinizi buraya girebilirsiniz....',
+    'meta_description' => 'Meta Açıklama',
+    'meta_keywords' => 'Meta Anahtar Kelimeler',
+    'meta_title' => 'Meta Başlığı',
+    'metas' => 'SEO',
+    'name' => 'Adı',
+    'open' => 'Açık',
+    'page' => 'sayfa',
+    'page_name' => 'Sayfa adı (Yalnızca yöneticiler tarafından görünür.)',
+    'page_slug' => 'Sayfa URL',
+    'page_slug_hint' => 'Boş bırakıldığında sayfa adından otomatik olarak oluşturulacaktır.',
+    'page_title' => 'Sayfa Başlığı',
+    'pages' => 'sayfalar',
+    'slug' => 'Slug',
+    'template' => 'Şablon',
+    'template_not_found' => 'Şablon bulunamadı. Bu sayfa oluşturulmadan önce silinmiş olabilir. Lütfen geliştirici ekibinizden bunu düzeltmesini isteyin.',
+];
