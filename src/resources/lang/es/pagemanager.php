af19dec098022ff2cd7976284f015ae302937bf5
Enrique García <enrique@inodata.com.mx>
2020-11-09 20:21:38 -0600

    Adds spanish translation file

---
+++ src/resources/lang/es/pagemanager.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'change_template_confirmation' => '¿Estás seguro de actualizar la plantilla de la página? Perderás los cambios que no hayan sido guardados para esta página.',
+    'content' => 'Contenido',
+    'content_placeholder' => 'Tu contenido aquí',
+    'meta_description' => 'Meta Descripción',
+    'meta_keywords' => 'Palabras Clave',
+    'meta_title' => 'Meta Título',
+    'metas' => 'Optimización de Búsquedas (SEO)',
+    'name' => 'Nombre',
+    'open' => 'Abierto',
+    'page' => 'página',
+    'page_name' => 'Nombre de la página (visible solo para administradores)',
+    'page_slug' => '"Slug" de la página (URL)',
+    'page_slug_hint' => 'Será generado automáticamente desde el título, si se deja vacío.',
+    'page_title' => 'Título de Página',
+    'pages' => 'páginas',
+    'slug' => 'Slug',
+    'template' => 'Plantilla',
+    'template_not_found' => 'La plantilla no pudo ser encontrada. Pudo haber sido eliminada desde que se creó esta página. Para continuar, por favor contacta al administrador o desarrollador para solucionarlo.',
+];
