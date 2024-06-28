bdd787eef2af5a78aef7555104403ad3d611bb1c
truongbo <truongnq017@gmail.com>
2024-06-28 22:21:37 +0700

    add support language vietnamese

---
+++ src/resources/lang/vi/pagemanager.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'change_template_confirmation' => 'Bạn có chắc muốn thay đổi mẫu trang? Mọi thay đổi sẽ không được lưu cho trang này.',
+    'content' => 'Nội dung',
+    'content_placeholder' => 'Nhập nội dung của bạn',
+    'meta_description' => 'Meta Description',
+    'meta_keywords' => 'Meta Keywords',
+    'meta_title' => 'Meta Title',
+    'metas' => 'Metas',
+    'name' => 'Tên',
+    'open' => 'Mở ra',
+    'page' => 'page',
+    'page_name' => 'Tiêu đề trang (chỉ hiển thị với admins)',
+    'page_slug' => 'Đường dẫn trang (URL)',
+    'page_slug_hint' => 'Đường dẫn sẽ tự động được tạo từ tiêu đề của bạn, nếu bạn để trống nó.',
+    'page_title' => 'Tiêu đề trang',
+    'pages' => 'pages',
+    'slug' => 'Đường dẫn',
+    'template' => 'Mẫu',
+    'template_not_found' => 'Không thể tìm thấy mẫu này. Nó có thể đã bị xóa kể từ khi trang này được tạo ra. Để tiếp tục, vui lòng yêu cầu webmin hoặc nhóm phát triển của bạn khắc phục lỗi này.',
+];
